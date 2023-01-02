169211d10b3bb5904d35ea85d5fea966fdda7bd1
Md Iqbal Hossain <97906560+iqbalhossain038@users.noreply.github.com>
2023-01-02 16:15:31 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//bx slider
+
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        auto: true,
+    });    
+    jQuery('.menuBar').meanmenu({
+        meanMenuOpen: "<span></span><span></span><span></span>",
+        meanMenuContainer: '.menu_plg',
+        meanScreenWidth: 768,
+    });
+});
